49c939a77323b7b11dde40b04519af962d461b13
Stefan Gohlke <stefan.gohlke@dreamit.de>
2022-04-28 17:58:44 +0200

    19 Provide removeHeader in GraphQLServerResponse.ts

--- src/server/GraphQLServerResponse.ts
+++ src/server/GraphQLServerResponse.ts
@@ -1,5 +1,6 @@
-export interface GraphQLServerResponse {   
+export interface GraphQLServerResponse {
     statusCode: number,
     setHeader(name: string, value: number | string | ReadonlyArray<string>): this
     end(chunk: unknown, callback?: () => void): this
+    removeHeader(name: string): void;
 }
