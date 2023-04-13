d2403f64629c7d27c6c9ec8d87f369a1201950eb
Stefan Gohlke <stefan.gohlke@dreamit.de>
2023-04-13 11:08:39 +0200

    157 Remove ResponseParameters interface, import it from @sgohlke/graphql-server-base

--- src/response/ResponseHandler.ts
+++ src/response/ResponseHandler.ts
@@ -1,14 +1,7 @@
 import {
     GraphQLExecutionResult,
-    GraphQLServerRequest,
-    GraphQLServerResponse,
-    Logger
+    ResponseParameters
 } from '@sgohlke/graphql-server-base'
-import {
-    ExecutionResult,
-    GraphQLError,
-    GraphQLFormattedError
-} from 'graphql'
 
 /**
  * Interface for ResponseHandler.
@@ -22,15 +15,4 @@ export interface ResponseHandler {
 
     /** Sends a response */
     sendResponse(responseParameters: ResponseParameters): void
-}
-
-export interface ResponseParameters {
-    readonly response: GraphQLServerResponse,
-    readonly context: unknown,
-    readonly logger: Logger
-    readonly formatErrorFunction: (error: GraphQLError) => GraphQLFormattedError
-    readonly request?: GraphQLServerRequest,
-    readonly customHeaders?: Record<string, string>,
-    readonly executionResult?: ExecutionResult,
-    readonly statusCode?: number,
 }
