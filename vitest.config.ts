2fb8e85739e697b7e9e2c1c985ec0cecf64d1b2b
sgohlke <gohlke.stefan@yahoo.de>
2024-07-15 22:23:26 +0200

    Adjust vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     resolve: { alias: { graphql: 'graphql/index.js' } },
     test: {
         coverage: {
-            exclude: ['.stryker-tmp', 'build', 'tests'],
+            exclude: ['.stryker-tmp', 'tsup.config.ts', 'vitest.config.ts', 'build', 'tests'],
             provider: 'v8',
             reporter: ['text'],
         },
