e75b6e79024a6c247b2fdc6c041f2c914586a23c
sgohlke <gohlke.stefan@yahoo.de>
2025-05-07 12:31:02 +0200

    Change to data driven test

--- tests/request/GetContentType.test.ts
+++ tests/request/GetContentType.test.ts
@@ -1,12 +1,16 @@
 import { getContentType } from 'src'
 import { expect, test } from 'vitest'
 
-test('Should get correct content type', () => {
-    expect(getContentType()).toBe('')
-    expect(getContentType('application/graphql')).toBe('application/graphql')
-    expect(getContentType('application/json')).toBe('application/json')
-    expect(getContentType('application/x-www-form-urlencoded')).toBe(
-        'application/x-www-form-urlencoded',
-    )
-    expect(getContentType('application/unknown')).toBe('')
-})
+test.each`
+    contentTypeAsString                    | expectedContentType
+    ${undefined}                           | ${''}
+    ${'application/graphql'}               | ${'application/graphql'}
+    ${'application/json'}                  | ${'application/json'}
+    ${'application/x-www-form-urlencoded'} | ${'application/x-www-form-urlencoded'}
+    ${'application/unknown'}               | ${''}
+`(
+    'Should get correct content type for given value $contentTypeAsString',
+    ({ contentTypeAsString, expectedContentType }) => {
+        expect(getContentType(contentTypeAsString)).toBe(expectedContentType)
+    },
+)
